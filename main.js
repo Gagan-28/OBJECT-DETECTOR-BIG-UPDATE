6ad7390c9a3e7f6e07c886ee15c3c5b60bafc013
Gagan-28 <84262680+Gagan-28@users.noreply.github.com>
2022-10-02 13:41:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function bedRoom()
+{
+    window.location = "bed_room.html";
+}
+function TVandAC()
+{
+    
+}
+function desk()
+{
+    
+}
+function bottles()
+{
+    
+}
+function fruitBasket()
+{
+    
+}
